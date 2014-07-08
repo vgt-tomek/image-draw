3d03c16343b24d33b472d405ee61276fd59417fd
VGT <tomek@vgtworld.pl>
2014-07-08 18:58:43 +0200

    Add image type to Image class.

--- image-draw/src/main/java/pl/vgtworld/imagedraw/processing/ImageProcessing.java
+++ image-draw/src/main/java/pl/vgtworld/imagedraw/processing/ImageProcessing.java
@@ -8,8 +8,11 @@ import java.io.InputStream;
 import javax.imageio.ImageIO;
 
 import pl.vgtworld.imagedraw.Image;
+import pl.vgtworld.imagedraw.ImageType;
 
 public class ImageProcessing {
+	
+	private static final ImageType DEFAULT_IMAGE_TYPE = ImageType.JPEG;
 	
 	public Image open(String path) throws IOException {
 		return open(new File(path));
@@ -17,12 +20,16 @@ public class ImageProcessing {
 	
 	public Image open(File file) throws IOException {
 		BufferedImage bufferedImage = ImageIO.read(file);
-		return new Image(bufferedImage);
+		return new Image(bufferedImage, DEFAULT_IMAGE_TYPE); //TODO Load image type from extension.
 	}
 	
 	public Image open(InputStream stream) throws IOException {
+		return open(stream, DEFAULT_IMAGE_TYPE);
+	}
+	
+	public Image open(InputStream stream, ImageType imageType) throws IOException {
 		BufferedImage bufferedImage = ImageIO.read(stream);
-		return new Image(bufferedImage);
+		return new Image(bufferedImage, imageType);
 	}
 	
 }
