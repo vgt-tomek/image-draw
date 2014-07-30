209798bbcca66606a3b9f04a97f83d0cbea76a46
VGT <tomek@vgtworld.pl>
2014-07-30 19:17:34 +0200

    Add test for transparency.

--- image-draw/src/test/java/pl/vgtworld/imagedraw/processing/ImageDrawActionsTest.java
+++ image-draw/src/test/java/pl/vgtworld/imagedraw/processing/ImageDrawActionsTest.java
@@ -4,6 +4,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import java.awt.Color;
 import java.awt.image.BufferedImage;
+import java.io.File;
 import java.io.IOException;
 
 import javax.imageio.ImageIO;
@@ -31,4 +32,23 @@ public class ImageDrawActionsTest {
 		assertThat(secondColumnPixel).isEqualTo(Color.RED);
 	}
 	
+	@Test
+	public void shouldDrawImageWithAlphaChannel() throws IOException {
+		BufferedImage sourceImage = ImageIO.read(getClass().getResourceAsStream("/alpha-test.png"));
+		BufferedImage destinationImage = ImageIO.read(getClass().getResourceAsStream("/image-100-100.jpg"));
+		ImageDrawEntity source = new ImageDrawEntity(sourceImage, ImageType.PNG);
+		ImageDrawEntity destination = new ImageDrawEntity(destinationImage, ImageType.JPEG);
+		ImageDrawActions actions = new ImageDrawActions();
+		
+		actions.drawImage(source, destination, 0, 0);
+		Color transparentPixel = new Color(destination.getImage().getRGB(5, 5));
+		Color redPixel = new Color(destination.getImage().getRGB(15, 5));
+		Color greenPixel = new Color(destination.getImage().getRGB(15, 15));
+		Color bluePixel = new Color(destination.getImage().getRGB(5, 15));
+		
+		assertThat(transparentPixel).isNotIn(Color.RED, Color.GREEN, Color.BLUE);
+		assertThat(redPixel).isEqualTo(Color.RED);
+		assertThat(greenPixel).isEqualTo(Color.GREEN);
+		assertThat(bluePixel).isEqualTo(Color.BLUE);
+	}
 }
