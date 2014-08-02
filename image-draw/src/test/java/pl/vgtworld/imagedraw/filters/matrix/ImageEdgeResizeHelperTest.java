e0b15ff5b82a281c2ec07dd2f4fbac8bb8e2a9a3
VGT <tomek@vgtworld.pl>
2014-08-02 21:53:40 +0200

    Change unit tests to work with rectangular instead of square image.

--- image-draw/src/test/java/pl/vgtworld/imagedraw/filters/matrix/ImageEdgeResizeHelperTest.java
+++ image-draw/src/test/java/pl/vgtworld/imagedraw/filters/matrix/ImageEdgeResizeHelperTest.java
@@ -11,11 +11,13 @@ import org.junit.Test;
 
 public class ImageEdgeResizeHelperTest {
 	
-	private static final int IMAGE_WIDTH_HEIGHT = 100;
+	private static final int IMAGE_WIDTH = 300;
+	
+	private static final int IMAGE_HEIGHT = 100;
 	
 	private static final int ADDITIONAL_EDGE_SIZE = 5;
 	
-	private static final String IMAGE_PATH = "/image-100-100.jpg";
+	private static final String IMAGE_PATH = "/image-300-100.jpg";
 	
 	private ImageEdgeResizeHelper helper = new ImageEdgeResizeHelper();
 	
@@ -25,9 +27,9 @@ public class ImageEdgeResizeHelperTest {
 		
 		BufferedImage result = helper.addEdgesToImage(sourceImage, 5);
 		
-		assertThat(result.getWidth()).isEqualTo(IMAGE_WIDTH_HEIGHT + 2 * ADDITIONAL_EDGE_SIZE);
-		assertThat(result.getHeight()).isEqualTo(IMAGE_WIDTH_HEIGHT + 2 * ADDITIONAL_EDGE_SIZE);
-		for (int i = 0; i < IMAGE_WIDTH_HEIGHT + 2 * ADDITIONAL_EDGE_SIZE; ++i) {
+		assertThat(result.getWidth()).isEqualTo(IMAGE_WIDTH + 2 * ADDITIONAL_EDGE_SIZE);
+		assertThat(result.getHeight()).isEqualTo(IMAGE_HEIGHT + 2 * ADDITIONAL_EDGE_SIZE);
+		for (int i = 0; i < IMAGE_WIDTH + 2 * ADDITIONAL_EDGE_SIZE; ++i) {
 			int firstPixel = result.getRGB(i, 0);
 			for (int j = 1; j <= ADDITIONAL_EDGE_SIZE; ++j) {
 				assertThat(result.getRGB(i, j)).isEqualTo(firstPixel);
@@ -41,11 +43,11 @@ public class ImageEdgeResizeHelperTest {
 		
 		BufferedImage result = helper.addEdgesToImage(sourceImage, ADDITIONAL_EDGE_SIZE);
 		
-		assertThat(result.getWidth()).isEqualTo(IMAGE_WIDTH_HEIGHT + 2 * ADDITIONAL_EDGE_SIZE);
-		assertThat(result.getHeight()).isEqualTo(IMAGE_WIDTH_HEIGHT + 2 * ADDITIONAL_EDGE_SIZE);
-		for (int i = 0; i < IMAGE_WIDTH_HEIGHT + 2 * ADDITIONAL_EDGE_SIZE; ++i) {
-			int firstPixel = result.getRGB(i, IMAGE_WIDTH_HEIGHT + ADDITIONAL_EDGE_SIZE - 1);
-			for (int j = IMAGE_WIDTH_HEIGHT + ADDITIONAL_EDGE_SIZE; j < IMAGE_WIDTH_HEIGHT + 2 * ADDITIONAL_EDGE_SIZE; ++j) {
+		assertThat(result.getWidth()).isEqualTo(IMAGE_WIDTH + 2 * ADDITIONAL_EDGE_SIZE);
+		assertThat(result.getHeight()).isEqualTo(IMAGE_HEIGHT + 2 * ADDITIONAL_EDGE_SIZE);
+		for (int i = 0; i < IMAGE_WIDTH + 2 * ADDITIONAL_EDGE_SIZE; ++i) {
+			int firstPixel = result.getRGB(i, IMAGE_HEIGHT + ADDITIONAL_EDGE_SIZE - 1);
+			for (int j = IMAGE_HEIGHT + ADDITIONAL_EDGE_SIZE; j < IMAGE_HEIGHT + 2 * ADDITIONAL_EDGE_SIZE; ++j) {
 				assertThat(result.getRGB(i, j)).isEqualTo(firstPixel);
 			}
 		}
@@ -57,9 +59,9 @@ public class ImageEdgeResizeHelperTest {
 		
 		BufferedImage result = helper.addEdgesToImage(sourceImage, ADDITIONAL_EDGE_SIZE);
 		
-		assertThat(result.getWidth()).isEqualTo(IMAGE_WIDTH_HEIGHT + 2 * ADDITIONAL_EDGE_SIZE);
-		assertThat(result.getHeight()).isEqualTo(IMAGE_WIDTH_HEIGHT + 2 * ADDITIONAL_EDGE_SIZE);
-		for (int j = 0; j < IMAGE_WIDTH_HEIGHT + 2 * ADDITIONAL_EDGE_SIZE; ++j) {
+		assertThat(result.getWidth()).isEqualTo(IMAGE_WIDTH + 2 * ADDITIONAL_EDGE_SIZE);
+		assertThat(result.getHeight()).isEqualTo(IMAGE_HEIGHT + 2 * ADDITIONAL_EDGE_SIZE);
+		for (int j = 0; j < IMAGE_HEIGHT + 2 * ADDITIONAL_EDGE_SIZE; ++j) {
 			int firstPixel = result.getRGB(0, j);
 			for (int i = 1; i <= ADDITIONAL_EDGE_SIZE; ++i) {
 				assertThat(result.getRGB(i, j)).isEqualTo(firstPixel);
@@ -73,11 +75,11 @@ public class ImageEdgeResizeHelperTest {
 		
 		BufferedImage result = helper.addEdgesToImage(sourceImage, ADDITIONAL_EDGE_SIZE);
 		
-		assertThat(result.getWidth()).isEqualTo(IMAGE_WIDTH_HEIGHT + 2 * ADDITIONAL_EDGE_SIZE);
-		assertThat(result.getHeight()).isEqualTo(IMAGE_WIDTH_HEIGHT + 2 * ADDITIONAL_EDGE_SIZE);
-		for (int j = 0; j < IMAGE_WIDTH_HEIGHT + 2 * ADDITIONAL_EDGE_SIZE; ++j) {
-			int firstPixel = result.getRGB(IMAGE_WIDTH_HEIGHT + ADDITIONAL_EDGE_SIZE - 1, j);
-			for (int i = IMAGE_WIDTH_HEIGHT + ADDITIONAL_EDGE_SIZE; i < IMAGE_WIDTH_HEIGHT + 2 * ADDITIONAL_EDGE_SIZE; ++i) {
+		assertThat(result.getWidth()).isEqualTo(IMAGE_WIDTH + 2 * ADDITIONAL_EDGE_SIZE);
+		assertThat(result.getHeight()).isEqualTo(IMAGE_HEIGHT + 2 * ADDITIONAL_EDGE_SIZE);
+		for (int j = 0; j < IMAGE_HEIGHT + 2 * ADDITIONAL_EDGE_SIZE; ++j) {
+			int firstPixel = result.getRGB(IMAGE_WIDTH + ADDITIONAL_EDGE_SIZE - 1, j);
+			for (int i = IMAGE_WIDTH + ADDITIONAL_EDGE_SIZE; i < IMAGE_WIDTH + 2 * ADDITIONAL_EDGE_SIZE; ++i) {
 				assertThat(result.getRGB(i, j)).isEqualTo(firstPixel);
 			}
 		}
