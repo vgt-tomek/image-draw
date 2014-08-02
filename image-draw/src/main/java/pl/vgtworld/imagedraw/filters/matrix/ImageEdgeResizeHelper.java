310085c54f84e03d1c086d3ed21f12f213c26e71
VGT <tomek@vgtworld.pl>
2014-08-02 21:50:56 +0200

    Remove unused code.

--- image-draw/src/main/java/pl/vgtworld/imagedraw/filters/matrix/ImageEdgeResizeHelper.java
+++ image-draw/src/main/java/pl/vgtworld/imagedraw/filters/matrix/ImageEdgeResizeHelper.java
@@ -2,9 +2,6 @@ package pl.vgtworld.imagedraw.filters.matrix;
 
 import java.awt.Graphics2D;
 import java.awt.image.BufferedImage;
-import java.io.IOException;
-
-import javax.imageio.ImageIO;
 
 class ImageEdgeResizeHelper {
 	
@@ -22,20 +19,8 @@ class ImageEdgeResizeHelper {
 			graphics.copyArea(edgeSize, 0, 1, finalImage.getHeight(), -i, 0);
 			graphics.copyArea(sourceImage.getWidth() + edgeSize - 1, 0, 1, finalImage.getWidth(), i, 0);
 		}
-		
 		graphics.dispose();
-		save(finalImage);
 		return finalImage;
 	}
 
-	//TODO remove
-	private void save(BufferedImage finalImage) {
-		try {
-			ImageIO.write(finalImage, "jpg",  new java.io.File("/home/vgt/Pictures/test/output.jpg"));
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-	}
-	
 }
