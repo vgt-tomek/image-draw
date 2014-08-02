a3cd50c38b7a003ec0a8086120694cc960608c28
VGT <tomek@vgtworld.pl>
2014-08-02 21:28:14 +0200

    Create edge resize helper dummy and unit tests.

---
+++ image-draw/src/main/java/pl/vgtworld/imagedraw/filters/matrix/ImageEdgeResizeHelper.java
@@ -0,0 +1,11 @@
+package pl.vgtworld.imagedraw.filters.matrix;
+
+import java.awt.image.BufferedImage;
+
+class ImageEdgeResizeHelper {
+	
+	BufferedImage addEdgesToImage(BufferedImage sourceImage, int edgeSize) {
+		throw new UnsupportedOperationException();
+	}
+	
+}
