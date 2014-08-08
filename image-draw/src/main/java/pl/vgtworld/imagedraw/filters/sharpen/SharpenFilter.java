49048430fcddbf051316f74e2f67069b893124a7
VGT <tomek@vgtworld.pl>
2014-08-08 20:16:13 +0200

    Add documentation.

--- image-draw/src/main/java/pl/vgtworld/imagedraw/filters/sharpen/SharpenFilter.java
+++ image-draw/src/main/java/pl/vgtworld/imagedraw/filters/sharpen/SharpenFilter.java
@@ -4,13 +4,27 @@ import pl.vgtworld.imagedraw.ImageDrawEntity;
 import pl.vgtworld.imagedraw.filters.ImageDrawFilter;
 import pl.vgtworld.imagedraw.filters.matrix.MatrixFilter;
 
+/**
+ * Filter applying sharpening to image.
+ */
 public class SharpenFilter implements ImageDrawFilter {
 	
 	private float strength = 1;
-	
+
+	/**
+	 * Creates filter with default configuration.
+	 * 
+	 * <p>
+	 * Sharpening strength is set to 1.
+	 */
 	public SharpenFilter() {
 	}
 	
+	/**
+	 * Creates filter with custom configuration.
+	 * 
+	 * @param strength Sharpening strength.
+	 */
 	public SharpenFilter(float strength) {
 		this.strength = strength;
 	}
