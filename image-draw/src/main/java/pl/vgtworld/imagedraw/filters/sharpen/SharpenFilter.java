342d73109ac77663360da88c00cc74d41a6904b3
VGT <tomek@vgtworld.pl>
2014-08-08 20:13:07 +0200

    Add strength parameter.

--- image-draw/src/main/java/pl/vgtworld/imagedraw/filters/sharpen/SharpenFilter.java
+++ image-draw/src/main/java/pl/vgtworld/imagedraw/filters/sharpen/SharpenFilter.java
@@ -6,12 +6,21 @@ import pl.vgtworld.imagedraw.filters.matrix.MatrixFilter;
 
 public class SharpenFilter implements ImageDrawFilter {
 	
+	private float strength = 1;
+	
+	public SharpenFilter() {
+	}
+	
+	public SharpenFilter(float strength) {
+		this.strength = strength;
+	}
+	
 	@Override
 	public void doFilter(ImageDrawEntity image, int x, int y, int width, int height) {
 		float[][] matrix = {
-				{ 0, -1, 0 },
-				{ -1, 5, -1 },
-				{ 0, -1, 0 }
+				{ -strength, -strength, -strength },
+				{ -strength, 1 + 8 * strength, -strength },
+				{ -strength, -strength, -strength }
 		};
 		MatrixFilter filter = new MatrixFilter(matrix);
 		filter.doFilter(image, x, y, width, height);
