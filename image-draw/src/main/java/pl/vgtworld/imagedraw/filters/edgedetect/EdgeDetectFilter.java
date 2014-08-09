a3d8ecec774d6c4b65efeec50eb5305099649ca4
VGT <tomek@vgtworld.pl>
2014-08-09 19:19:51 +0200

    Add documentation.

--- image-draw/src/main/java/pl/vgtworld/imagedraw/filters/edgedetect/EdgeDetectFilter.java
+++ image-draw/src/main/java/pl/vgtworld/imagedraw/filters/edgedetect/EdgeDetectFilter.java
@@ -4,16 +4,30 @@ import pl.vgtworld.imagedraw.ImageDrawEntity;
 import pl.vgtworld.imagedraw.filters.ImageDrawFilter;
 import pl.vgtworld.imagedraw.filters.matrix.MatrixFilter;
 
+/**
+ * Filter applying edge detection to image.
+ */
 public class EdgeDetectFilter implements ImageDrawFilter {
 	
 	private static final int DEFAULT_STRENGTH = 1;
 	
 	private float strength;
 	
+	/**
+	 * Creates filter with default configuration.
+	 * 
+	 * <p>
+	 * Edge detection strength is set to 1.
+	 */
 	public EdgeDetectFilter() {
 		strength = DEFAULT_STRENGTH;
 	}
 	
+	/**
+	 * Creates filter with custom configuration.
+	 * 
+	 * @param strength Edge detection strength.
+	 */
 	public EdgeDetectFilter(float strength) {
 		this.strength = strength;
 	}
